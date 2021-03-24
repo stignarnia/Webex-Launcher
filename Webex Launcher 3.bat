d7c27bd23418365ae6070e4133069a827138ae16
stignarnia <80171209+stignarnia@users.noreply.github.com>
2021-03-25 00:17:50 +0100

    Update Webex Launcher 3.bat

--- Webex Launcher 3.bat
+++ Webex Launcher 3.bat
@@ -5,12 +5,10 @@ call config.bat
 @Title %~n0
 Batbox /h 0
 
-:Loop
-Call Button  4 4 %pulsante8% 30 4 %pulsante9% 17 8 %pulsante0% # Press
-Getinput /m %Press% /h 70
+call Button.bat  4 3 %pulsante8% 30 3 %pulsante9% 17 9 %pulsante0% # Press
+call InputTranslator.bat
 
 :: Check for the pressed button 
-if %errorlevel%==1 (start prof8)
-if %errorlevel%==2 (start prof9)
-if %errorlevel%==3 (exit)
-goto Loop
+if %bottone%==1 (start prof8)
+if %bottone%==2 (start prof9)
+if %bottone%==3 (exit)
